1c7ded0e64da3dca1f2c4d2faf3b8b5269b4b19b
Clément Bartholomé <clementoss@gmail.com>
2023-11-07 13:04:02 +0100

    fix logos images file path

--- src/assets/sources.ts
+++ src/assets/sources.ts
@@ -2,37 +2,37 @@ export const sources = [
   {
     name: 'BasketUSA',
     url: 'https://www.basketusa.com/feed',
-    logo: '../public/images/basketusa.webp'
+    logo: 'https://raw.githubusercontent.com/ClementBartholome/BasketNews/main/public/images/basketusa.webp'
   },
   { name: 'TrashTalk', url: 'https://trashtalk.co/feed', logo: '../public/images/trashtalk.png' },
   {
     name: 'BasketSession',
     url: 'https://www.basketsession.com/feed',
-    logo: '../public/images/basketsession.webp'
+    logo: 'https://raw.githubusercontent.com/ClementBartholome/BasketNews/main/public/images/basketsession.webp'
   },
   {
     name: 'BasketEurope',
     url: 'https://www.basketeurope.com/feed',
-    logo: '../public/images/basketeurope.webp'
+    logo: 'https://raw.githubusercontent.com/ClementBartholome/BasketNews/main/public/images/basketeurope.webp'
   },
   {
     name: "L'Equipe",
     url: 'https://dwh.lequipe.fr/api/edito/rss?path=/Basket',
-    logo: '../public/images/lequipe.webp'
+    logo: 'https://raw.githubusercontent.com/ClementBartholome/BasketNews/main/public/images/lequipe.webp'
   },
   {
     name: 'ESPN NBA',
     url: 'https://www.espn.com/espn/rss/nba/news',
-    logo: '../public/images/espn.png'
+    logo: 'https://raw.githubusercontent.com/ClementBartholome/BasketNews/main/public/images/espn.png'
   },
   {
     name: 'CBS Sports NBA',
     url: 'https://www.cbssports.com/rss/headlines/nba',
-    logo: '../public/images/cbs.webp'
+    logo: 'https://raw.githubusercontent.com/ClementBartholome/BasketNews/main/public/images/cbs.webp'
   },
   {
     name: 'New York Times',
     url: 'https://rss.nytimes.com/services/xml/rss/nyt/ProBasketball.xml',
-    logo: '../public/images/nytimes.png'
+    logo: 'https://raw.githubusercontent.com/ClementBartholome/BasketNews/main/public/images/nytimes.png'
   }
 ]
