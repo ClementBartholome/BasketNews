c2e925bd7ec01710bce9b612c5a6c3bca7661d99
Clément Bartholomé <clementoss@gmail.com>
2023-11-07 12:58:59 +0100

    fix sources logos file path

--- src/assets/sources.ts
+++ src/assets/sources.ts
@@ -2,37 +2,37 @@ export const sources = [
   {
     name: 'BasketUSA',
     url: 'https://www.basketusa.com/feed',
-    logo: '/images/basketusa.webp'
+    logo: '../public/images/basketusa.webp'
   },
-  { name: 'TrashTalk', url: 'https://trashtalk.co/feed', logo: '/images/trashtalk.png' },
+  { name: 'TrashTalk', url: 'https://trashtalk.co/feed', logo: '../public/images/trashtalk.png' },
   {
     name: 'BasketSession',
     url: 'https://www.basketsession.com/feed',
-    logo: '/images/basketsession.webp'
+    logo: '../public/images/basketsession.webp'
   },
   {
     name: 'BasketEurope',
     url: 'https://www.basketeurope.com/feed',
-    logo: '/images/basketeurope.webp'
+    logo: '../public/images/basketeurope.webp'
   },
   {
     name: "L'Equipe",
     url: 'https://dwh.lequipe.fr/api/edito/rss?path=/Basket',
-    logo: '/images/lequipe.webp'
+    logo: '../public/images/lequipe.webp'
   },
   {
     name: 'ESPN NBA',
     url: 'https://www.espn.com/espn/rss/nba/news',
-    logo: '/images/espn.png'
+    logo: '../public/images/espn.png'
   },
   {
     name: 'CBS Sports NBA',
     url: 'https://www.cbssports.com/rss/headlines/nba',
-    logo: '/images/cbs.webp'
+    logo: '../public/images/cbs.webp'
   },
   {
     name: 'New York Times',
     url: 'https://rss.nytimes.com/services/xml/rss/nyt/ProBasketball.xml',
-    logo: '/images/nytimes.png'
+    logo: '../public/images/nytimes.png'
   }
 ]
