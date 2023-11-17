65f05a8f4c66b80351a647a876e9eace7384258c
Clément Bartholomé <clementoss@gmail.com>
2023-11-17 23:34:33 +0100

    feat: create user store for login / logout via my .NET backend

---
+++ src/stores/user.ts
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+  state: () => ({
+    user: JSON.parse(localStorage.getItem('user')) || null
+  }),
+
+  getters: {
+    isLoggedIn: (state) => !!state.user,
+    getUser: (state) => state.user
+  },
+
+  actions: {
+    async login(email: string, password: string, confirmPassword: string) {
+      const response = await axios.post('https://localhost:7172/api/Account/login', {
+        email,
+        password,
+        confirmPassword
+      })
+
+      const user = response.data
+      this.user = user
+      localStorage.setItem('user', JSON.stringify(user))
+      return user
+    },
+    async logout() {
+      this.user = null
+      localStorage.removeItem('user')
+    }
+  }
+})
