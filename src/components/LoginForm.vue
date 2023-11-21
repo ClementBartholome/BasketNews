5bc34fc6eb919c36773e269ffab108bd046153bc
Clément Bartholomé <clementoss@gmail.com>
2023-11-21 10:08:07 +0100

    add link to register

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,20 +1,37 @@
 <template>
-  <form @submit.prevent="loginUser">
-    <label>Email:</label>
-    <input type="email" v-model="loginData.email" required />
+  <main>
+    <h2>Connexion</h2>
+    <form @submit.prevent="loginUser">
+      <label>Email:</label>
+      <input type="email" autocomplete="username" v-model="loginData.email" required />
 
-    <label>Mot de passe:</label>
-    <input type="password" v-model="loginData.password" required />
+      <label>Mot de passe:</label>
+      <input
+        type="password"
+        autocomplete="current-password"
+        v-model="loginData.password"
+        required
+      />
 
-    <label>Confirmer le mot de passe:</label>
-    <input type="password" v-model="loginData.confirmPassword" required />
+      <label>Confirmer le mot de passe:</label>
+      <input
+        type="password"
+        autocomplete="current-password"
+        v-model="loginData.confirmPassword"
+        required
+      />
 
-    <button type="submit">Se connecter</button>
+      <button type="submit">Se connecter</button>
 
-    <div v-if="catchError">
-      {{ catchError }}
-    </div>
-  </form>
+      <p v-if="catchError">
+        {{ catchError }}
+      </p>
+      <p>
+        Pas de compte ?
+        <router-link to="/register" class="register-link">Inscrivez-vous</router-link>
+      </p>
+    </form>
+  </main>
 </template>
 
 <script setup lang="ts">
@@ -55,3 +72,49 @@ const loginUser = async () => {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+h2 {
+  position: absolute;
+  left: 40%;
+  @media (max-width: 500px) {
+    position: initial;
+  }
+}
+form {
+  position: absolute;
+  bottom: 40%;
+  left: 40%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 250px;
+
+  @media (max-width: 500px) {
+    position: initial;
+  }
+
+  input {
+    padding: 5px;
+  }
+  .register-link {
+    text-decoration: underline;
+
+    &:hover {
+      opacity: 0.7;
+    }
+  }
+}
+
+button {
+  color: var(--text-color);
+  border: 1px solid var(--text-color);
+  padding: 10px;
+
+  &:hover {
+    background-color: var(--text-color);
+    color: var(--background-color);
+    transition: background-color 0.6s;
+  }
+}
+</style>
