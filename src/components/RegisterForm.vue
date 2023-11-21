16d000fa5f8cf1903a468c10d524061c0a32139e
Clément Bartholomé <clementoss@gmail.com>
2023-11-21 10:08:19 +0100

    style

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,20 +1,28 @@
 <template>
-  <form @submit.prevent="registerUser">
-    <label>Email:</label>
-    <input type="email" v-model="registerData.email" required />
+  <main>
+    <h2>Inscription</h2>
+    <form @submit.prevent="registerUser">
+      <label>Email:</label>
+      <input type="email" autocomplete="username" v-model="registerData.email" required />
 
-    <label>Mot de passe:</label>
-    <input type="password" v-model="registerData.password" required />
+      <label>Mot de passe:</label>
+      <input type="password" autocomplete="new-password" v-model="registerData.password" required />
 
-    <label>Confirmer le mot de passe:</label>
-    <input type="password" v-model="registerData.confirmPassword" required />
+      <label>Confirmer le mot de passe:</label>
+      <input
+        type="password"
+        autocomplete="new-password"
+        v-model="registerData.confirmPassword"
+        required
+      />
 
-    <button type="submit">S'inscrire</button>
+      <button type="submit">S'inscrire</button>
 
-    <div v-if="catchError">
-      {{ catchError }}
-    </div>
-  </form>
+      <p v-if="catchError">
+        {{ catchError }}
+      </p>
+    </form>
+  </main>
 </template>
 
 <script setup lang="ts">
@@ -53,3 +61,41 @@ const registerUser = async () => {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+h2 {
+  position: absolute;
+  left: 40%;
+  @media (max-width: 500px) {
+    position: initial;
+  }
+}
+form {
+  position: absolute;
+  bottom: 40%;
+  left: 40%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  width: 400px;
+  @media (max-width: 500px) {
+    position: initial;
+  }
+
+  input {
+    padding: 5px;
+  }
+}
+
+button {
+  color: var(--text-color);
+  border: 1px solid var(--text-color);
+  padding: 10px;
+
+  &:hover {
+    background-color: var(--text-color);
+    color: var(--background-color);
+    transition: background-color 0.6s;
+  }
+}
+</style>
